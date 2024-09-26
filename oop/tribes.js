5d50202caee4b7a199b1bad31229ac9f537a78df
TominVadim <tomin.1991@inbox.ru>
2024-09-26 14:08:23 +0300

    26.09

--- oop/tribes.js
+++ oop/tribes.js
@@ -1,189 +1,186 @@
 // абстракция
 class TribeMember {
-    // создаем конструктор объекта
-    constructor(name) {
-        this.name = name;
-        this.age = Math.round(Math.random() * 100);
-        this.health = Math.round(Math.random() * 100);
-        this.tools = [];
-        this.damage = 7;
-    }
+  // создаем конструктор объекта
+  constructor(name) {
+    this.name = name;
+    this.age = Math.round(Math.random() * 100);
+    this.health = Math.round(Math.random() * 100);
+    this.tools = [];
+    this.damage = 7;
+  }
 
-    getInfo() {
-        console.log(`Абориген ${this.name}, ему ${this.age} лет и его здоровье равно ${this.health}`);
-    }
-    
-    takeDamage(damage) {
-        this.health -= damage;
-        console.log(`${this.name} получил ${damage} урона. Теперь его здоровье равно ${this.health}`);
-        if (this.health <= 0) {
-            console.log(`${this.name} убит`);
-            return true;
-        } else {
-            return false;
-        }
-    }
-    addTool(tool) {
-        this.tools.push(tool);
-    }
+  getInfo() {
+    console.log(`Абориген ${this.name}, ему ${this.age} лет и его здоровье равно ${this.health}`);
+  }
 
-    getToolList() {
-        const list = this.tools.map(({name, durability}) => `${name}, ${durability}`)
-        console.log(`${this.name} имеет в багаже: ${list.join('; ')}`);
+  takeDamage(damage) {
+    this.health -= damage;
+    console.log(`${this.name} получил ${damage} урона. Теперь его здоровье равно ${this.health}`);
+    if (this.health <= 0) {
+      console.log(`${this.name} убит`);
+      return true;
     }
-    attack(target) {
-        if(this.tools.length === 0) {
-            console.log(`${this.name} пошёл в рукопашную`);
-        } else {
-            console.log(`\n${this.name} атакует ${target.name} с помощью ${this.tools.at(0).name}`)
-        }
-        let tool = this.tools.at(0) || 0; // выбор предмета
-        let toolDamage = tool.damage;
+    return false;
+  }
 
-        if (tool.use() ?? false) {
-            let isKilled = target.takeDamage(this.damage + toolDamage);
-            if (isKilled) {
-                target = null;
-            }
+  addTool(tool) {
+    this.tools.push(tool);
+  }
+
+  getToolList() {
+    const list = this.tools.map(({ name, durability }) => `${name}, ${durability}`);
+    console.log(`${this.name} имеет в багаже: ${list.join('; ')}`);
+  }
+
+  attack(target) {
+    if (this.tools.length === 0) {
+      console.log(`${this.name} пошёл в рукопашную`);
+    } else {
+      console.log(`\n${this.name} атакует ${target.name} с помощью ${this.tools.at(0).name}`);
     }
-}
+    const tool = this.tools.at(0) || 0; // выбор предмета
+    const toolDamage = tool.damage;
+
+    if (tool.use() ?? false) {
+      const isKilled = target.takeDamage(this.damage + toolDamage);
+      if (isKilled) {
+        target = null;
+      }
+    }
+  }
 }
 // дочерние классы
 class Apache extends TribeMember {
-    constructor(name) {
-        // обращаемся к конструктору TribeMember
-        super(name);
-        this.className = 'apache';
-        this.farmingSkill = 60 + Math.round(Math.random() * 40);
-        if (this.health >= 40) {
-            this.health = Math.round(Math.random() * 40)
-        }
-        this.tools = [];
+  constructor(name) {
+    // обращаемся к конструктору TribeMember
+    super(name);
+    this.className = 'apache';
+    this.farmingSkill = 60 + Math.round(Math.random() * 40);
+    if (this.health >= 40) {
+      this.health = Math.round(Math.random() * 40);
     }
+    this.tools = [];
+  }
 
-    getDescription() {
-        this.getInfo();
-        console.log(`Абориген ${this.name} живет в племени Апаче, имеет навык земледелия ${this.farmingSkill}`);
-        this.getToolList()
-    }
-
-
+  getDescription() {
+    this.getInfo();
+    console.log(`Абориген ${this.name} живет в племени Апаче, имеет навык земледелия ${this.farmingSkill}`);
+    this.getToolList();
+  }
 }
 
 // создаем класс реднеков (здоровье выше 60, навык войны)
 class Redneck extends TribeMember {
-    constructor(name) {
-        //обращаемся к конструкутуру TribeMemeber
-        super(name);
-        this.className = 'redneck';
-        this.warSkill = 60 + Math.round(Math.random() * 40);
-        if (this.health >= 60) {
-            this.health = Math.round(Math.random() * 60);
-    
-        }
+  constructor(name) {
+    // обращаемся к конструкутуру TribeMemeber
+    super(name);
+    this.className = 'redneck';
+    this.warSkill = 60 + Math.round(Math.random() * 40);
+    if (this.health >= 60) {
+      this.health = Math.round(Math.random() * 60);
+    }
+  }
 
-    }
-    getDescription() {
-        this.getInfo();
-        console.log(`Абориген ${this.name} живёт в племени Реднеков имеет навык войны ${this.warSkill}`);
-        this.getToolList();
-    }
+  getDescription() {
+    this.getInfo();
+    console.log(`Абориген ${this.name} живёт в племени Реднеков имеет навык войны ${this.warSkill}`);
+    this.getToolList();
+  }
 }
 
 class Tomohavk extends Apache {
-    constructor(name){
-        super(name);
-        this.className = 'tomohavk';
-        this.dog = [];
-    }
-    addDog(dogName) {
-        this.dog.push(dogName);
-    }
-    useDog(target) {
-        this.dog.at(0).attack(target);
+  constructor(name) {
+    super(name);
+    this.className = 'tomohavk';
+    this.dog = [];
+  }
 
-    }
-    hasDog(target) {
-        if(this.tools.length === 0) {
-            console.log(`${this.name} выпускает собаку`);
-        this.useDog(target);
+  addDog(dogName) {
+    this.dog.push(dogName);
+  }
+
+  useDog(target) {
+    this.dog.at(0).attack(target);
+  }
+
+  hasDog(target) {
+    if (this.tools.length === 0) {
+      console.log(`${this.name} выпускает собаку`);
+      this.useDog(target);
     } else {
-        console.log(`${this.name} не доступна`);
+      console.log(`${this.name} не доступна`);
     }
+  }
 }
-} 
 
-//Valera.getDescription();
+// Valera.getDescription();
 
 // создадим класс оружия
 class Item {
-    constructor(name) {
-        this.name = name;
-        this.durability = Math.round(Math.random() * 5);
-        this.damage = 1;
-    }
+  constructor(name) {
+    this.name = name;
+    this.durability = Math.round(Math.random() * 5);
+    this.damage = 1;
+  }
 
-    use() {
-        if (this.durability > 0) {
-            this.durability -= 1;
-            console.log(`${this.name} использован. Осталось ${this.durability} использований`);
-            return true;
-        } else {
-            console.log(`${this.name} сломан, больше не используется.`);
-            return false;
-        }
+  use() {
+    if (this.durability > 0) {
+      this.durability -= 1;
+      console.log(`${this.name} использован. Осталось ${this.durability} использований`);
+      return true;
     }
-    
+    console.log(`${this.name} сломан, больше не используется.`);
+    return false;
+  }
 }
 
 class Weapon extends Item {
-    constructor(name) {
-        super(name);
-        this.className = 'weapon';
-        this.durability += 3;
-        this.damage += Math.round(Math.random() * 5);
-    }
+  constructor(name) {
+    super(name);
+    this.className = 'weapon';
+    this.durability += 3;
+    this.damage += Math.round(Math.random() * 5);
+  }
 }
 
 class Tools extends Item {
-    constructor(name) {
-        super(name);
-        this.className = 'tools';
-    }
-    
+  constructor(name) {
+    super(name);
+    this.className = 'tools';
+  }
 }
 
 class Dog {
-    constructor(name) {
-        this.name = name;
-        this.health = Math.round(Math.random() * 50);
-        this.damage = 6; 
+  constructor(name) {
+    this.name = name;
+    this.health = Math.round(Math.random() * 50);
+    this.damage = 6;
+  }
+
+  takeDamage(damage) {
+    this.health -= damage;
+    console.log(`${this.name} получил ${damage} урона. Теперь его здоровье равно ${this.health}`);
+    if (this.health <= 0) {
+      console.log(`${this.name} убит`);
+      return true;
     }
-    takeDamage(damage) {
-        this.health -= damage;
-        console.log(`${this.name} получил ${damage} урона. Теперь его здоровье равно ${this.health}`);
-        if (this.health <= 0) {
-            console.log(`${this.name} убит`);
-            return true;
-        } else {
-            return false;
-        }
+    return false;
+  }
+
+  attack(target) {
+    console.log(`\n${this.name} атакует ${target.name}`);
+    const isKilled = target.takeDamage(this.damage);
+    if (isKilled) {
+      target = null;
     }
-    attack(target) {
-        console.log(`\n${this.name} атакует ${target.name}`);
-        let isKilled = target.takeDamage(this.damage);
-            if (isKilled) {
-                target = null;
-            }
-    }
+  }
 }
 
 const Vitaly = new Apache('Vitaly');
 const Valera = new Redneck('Valera');
 
-
-//const Daniil = new TribeMember('Daniil');
-//console.log(Vitaly)
+// const Daniil = new TribeMember('Daniil');
+// console.log(Vitaly)
 
 const motiga = new Tools('motiga');
 const shovel = new Tools('shovel');
@@ -192,7 +189,7 @@ Vitaly.addTool(shovel);
 
 Vitaly.getDescription();
 
-const axe = new Weapon ('axe');
+const axe = new Weapon('axe');
 Valera.addTool(axe);
 Vitaly.attack(Valera);
 console.log(Valera);
@@ -203,4 +200,6 @@ const Cerber = new Dog('Cerber');
 Ludmila.addDog(Cerber);
 Ludmila.attack(Valera);
 
-export { TribeMember, Apache, Redneck, Tomohavk, Item, Weapon, Dog, Tools };
+export {
+  TribeMember, Apache, Redneck, Tomohavk, Item, Weapon, Dog, Tools,
+};
--- oop/utility.js
+++ oop/utility.js
@@ -1,74 +1,78 @@
 import fs from 'fs';
 import path from 'path';
 import readlineSync from 'readline-sync';
-import { TribeMember, Tomohavk, Apache, Redneck, Item, Weapon, Dog, Tools } from './tribes.js';
 import _ from 'lodash';
-const getPath = (fPath) => {
-    return path.resolve() + fPath;
-}
+import {
+  TribeMember, Tomohavk, Apache, Redneck, Item, Weapon, Dog, Tools,
+} from './tribes.js';
 
-const writeDownPeopleList = () => {
-    const fPath = getPath('/people.json');
-    return peopleList = JSON.parse(fs.readFileSync(fPath));
-}
+const getPath = (fPath) => path.resolve() + fPath;
+
+const readDownPeopleList = () => {
+  const fPath = getPath('/people.json');
+  const peopleList = JSON.parse(fs.readFileSync(fPath));
+  return peopleList;
+};
 const peopleListToString = () => {
-    fs.writeFileSync(fPath, JSON.stringify(peopleList, null, 2), 'utf-8');
-}
+  fs.writeFileSync(fPath, JSON.stringify(peopleList, null, 2), 'utf-8');
+};
 
 const setPerson = (person) => {
-    // узнаём путь, потом пушим в живых person
-    writeDownPeopleList();
-    peopleList.alive.push(person);
-    
-}
+  // узнаём путь, потом пушим в живых person
+  const peopleList = readDownPeopleList();
+  peopleList.alive.push(person);
+};
 const deleteDeadPerson = (person) => {
-    writeDownPeopleList();
-   
-    //const indexAlivePerson = peopleList.alive.map();
-    const namePerson = person.name;
-    const filtered = peopleList.alive.filter(({name}) => name !== namePerson)
-    peopleList.alive = filtered;
-    peopleListToString();
-}
-    
-   
+  readDownPeopleList();
+
+  // const indexAlivePerson = peopleList.alive.map();
+  const namePerson = person.name;
+  const filtered = peopleList.alive.filter(({ name }) => name !== namePerson);
+  peopleList.alive = filtered;
+  peopleListToString();
+};
+
 const updatePerson = (person) => {
-    writeDownPeopleList();
-    const namePerson = person.name;
-    const filtered = peopleList.alive.filter(({name}) => name !== namePerson)
-    peopleList.alive = filtered;
-    peopleList.alive.push(person);
-    peopleListToString();
-}
+  readDownPeopleList();
+  const namePerson = person.name;
+  const filtered = peopleList.alive.filter(({ name }) => name !== namePerson);
+  peopleList.alive = filtered;
+  peopleList.alive.push(person);
+  peopleListToString();
+};
 
-const backToClass = (name, className) => {
-    writeDownPeopleList();
-    const filtered = peopleList.alive.filter(({nameIter}) => name === nameIter).at(0);
-    let classObject;
-    switch (filtered.className) {
-        case 'apache':
-            classObject = new Apache(name);
-            break;
-        case 'redneck':
-            classObject = new Redneck(name);
-            break;
-        case 'weapon':
-            classObject = new Weapon(name);
-            break;
-        case 'tomohawk':
-            classObject = new Tomohavk(name);
-            break;
-        default:
-            classObject = new Tools(name);
-            break;
-    }
-    const entries = Object.entries(filtered);
-    for ([key, value] of entries) {
-        if (_.isObject(value)) {
-            classObject[key] = value.map((item) => backToClass(item));            
-        } else {
-        classObject[key] = value;
-        }
-    }
+const backToClass = (name) => {
+  readDownPeopleList();
+  const filtered = peopleList.alive.filter(({ nameIter }) => name === nameIter).at(0);
+  let classObject;
+  switch (filtered.className) {
+    case 'apache':
+      classObject = new Apache(name);
+      break;
+    case 'redneck':
+      classObject = new Redneck(name);
+      break;
+    case 'weapon':
+      classObject = new Weapon(name);
+      break;
+    case 'tomohawk':
+      classObject = new Tomohavk(name);
+      break;
+    default:
+      classObject = new Tools(name);
+      break;
+  }
+  const entries = Object.entries(filtered);
+  // for ([key, value] of entries) {
+  //     if (_.isObject(value)) {
+  //         classObject[key] = value.map((item) => backToClass(item));
+  //     } else {
+  //     classObject[key] = value;
+  //     }
+  // }
+  entries.forEach(([key, value]) => {
+    classObject[key] = _.isObject(value) ? value.map((item) => backToClass(item)) : value;
+  });
 
-}
+  return classObject;
+};
